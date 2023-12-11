48e99156fadac5bd1f4909932d41709f38615440
Messai <messai@github.com>
2023-12-11 08:28:47 +0300

    Some basic setups

--- api/Config/db.js
+++ api/Config/db.js
@@ -1,19 +1,23 @@
 // setting up db connection
 const mysql2 = require("mysql2");
-
+require("dotenv").config();
 
 const dbConnection = mysql2.createPool({
-  user: "newuser",
-  database: "note_app",
-  host: "localhost",
-  password: "0922597281aA!",
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASS,
   connectionLimit: 10,
 });
 
+
 dbConnection.execute("select 'test'", (err, result) => {
   if (err) {
+    console.log(process.env.DB_DATABASE)
     console.log(err.message);
   } else {
     console.log(result);
   }
 });
+
+module.exports = dbConnection;
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 3003
 // user router mdidleware file
 
 const userRoute = require('./api/routes/userRoute')
+const dbConnection = require("./api/Config/db");
 
 // user route middleware
 
@@ -19,7 +20,6 @@ app.listen(port,(err)=>{
   if (err){
     console.log(err.message)
   } else {
-    console.log(`Litsening on port ${port}`)
+    console.log(`Litsening on port ${port}`);
   }
 })
-
