b73de33026e138e9c1e8ca0b75c494aaf5034c61
Messai <messai@github.com>
2024-02-08 14:37:41 +0300

    Updated notesController by making it align with userscontroller

--- api/controller/notesController.js
+++ api/controller/notesController.js
@@ -1,26 +1,29 @@
-
 const { request } = require("express");
 const dbConnection = require("../Config/db");
 
+const postNotes = (req, res) => {
+  const text = req.body.text;
+  const user = req.user.id;
+  try {
+    dbConnection.query("INSERT INTO notes (user_id, note_text) VALUES (?,?)", [
+      user,
+      text,
+    ]);
+    res.send("Note Successfully Posted!!");
+  } catch (error) {
+    console.log("Unable to post your note");
+    res.send("Unable to post your note");
+  }
+};
 
-const postNotes=(req,res)=>{
-  const data = req.body
- try {
- dbConnection.query("INSERT INTO notes SET ?", data);
-  res.send("Note Successfully Posted!!")
- } catch (error) {
-  console.log("Unable to post your note")
-  res.send("Unable to post your note");
- }
-}
+const getNotes = async (req, res) => {
+  const user = req.user;
 
+  const results = await dbConnection.query(
+    "SELECT * FROM notes WHERE user_id=?",
+    user.id
+  );
+  res.send(results[0]);
+};
 
-
-const getNotes= (req,res)=>{
-
-}
-
-
-
-
-module.exports  ={postNotes,getNotes}
+module.exports = { postNotes, getNotes };
