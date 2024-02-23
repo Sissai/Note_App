ac1f2901cd496c0df455d014c4b664d5620980d6
Messai <messai@github.com>
2024-02-23 19:19:12 +0300

    Notes routes and controller

--- api/controller/notesController.js
+++ api/controller/notesController.js
@@ -26,4 +26,78 @@ const getNotes = async (req, res) => {
   res.send(results[0]);
 };
 
-module.exports = { postNotes, getNotes };
+const updateNote = (req, res) => {
+  const data = req.body;
+
+  const user = req.user;
+  try {
+    dbConnection.query(
+      "UPDATE notes SET note_text=? WHERE id=? AND user_id=?",
+      [data.text, data.id, user.id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({
+            status: false,
+            message: "Internal Server Error",
+          });
+        } else if (result["affectedRows"] === 0) {
+          res.status(400).json({
+            status: false,
+            message: "Could not update note",
+          });
+        } else {
+          console.log("Updated post successfully");
+          res.status(201).json({
+            status: true,
+            message: "Updated post successfully",
+          });
+        }
+      }
+    );
+  } catch (err) {
+    res.status(500).json({
+      status: false,
+      message: "Internal Server Error",
+    });
+    console.log(err.message);
+  }
+};
+
+const deleteNote = (req, res) => {
+  const data = req.body;
+
+  const user = req.user;
+
+  console.log(data, user);
+
+  dbConnection.execute(
+    "DELETE FROM notes WHERE id=? AND user_id=?",
+    [data.id, user.id],
+    (err, result) => {
+      if (err) {
+        console.log(err.message);
+        res.status(500).json({
+          status: false,
+          message: "Internal Server Error",
+        });
+      } else if (result["affectedRows"] === 0) {
+        console.log(
+          "No rows are deleted. You are either not authorized or the note doesn't exist"
+        );
+        res.status(400).json({
+          status: false,
+          message: "Could not delete the specified note",
+        });
+      } else {
+        res.status(200).json({
+          status: true,
+          message: "Deleted note successfully",
+        });
+        console.log("Deleted note successfully");
+      }
+    }
+  );
+};
+
+module.exports = { postNotes, getNotes, updateNote, deleteNote };
--- api/routes/notesRouter.js
+++ api/routes/notesRouter.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const {
   postNotes,
   getNotes,
-  updateNotes,
-  deleteNotes,
+  updateNote,
+  deleteNote,
 } = require("../controller/notesController");
 const { protect } = require("../controller/userController");
 
 // post notes
 router.post("/post", protect, postNotes);
 router.get("/get", protect, getNotes);
-// router.post("/update", protect, updateNotes);
-// router.delete("/delete", protect, deleteNotes);
+router.post("/update", protect, updateNote);
+router.delete("/delete", protect, deleteNote);
 
 module.exports = router;
