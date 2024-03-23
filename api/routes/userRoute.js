c60e232461ea7e34c0397fb7e594b2ba9efd7391
Messai <messai@github.com>
2024-03-23 14:52:55 +0300

    some changes

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,29 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const { postNotes, getNotes } = require("../controller/notesController");
 
 // user controller functions
 
-const{register,login,checkuser, logout}=require('../controller/userController.js')
+const {
+  register,
+  login,
+  checkuser,
+  logout,
+} = require("../controller/userController.js");
 
 // register route
 
-router.post('/register',register)
+router.post("/register", register);
 
 // login user
 
 router.post("/login", login);
 
-
-// check user
-
-router.get("/check", checkuser);
-
 // logout user
 
 router.get("/logout", logout);
 
-
-
-
-module.exports = router
+module.exports = router;
