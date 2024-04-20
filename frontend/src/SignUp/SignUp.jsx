e966e8a1cd3aea9a2a36f39a4a307671c526bb71
Sissai <139825952+Sissai@users.noreply.github.com>
2024-04-20 07:57:52 -0400

    Created Header and Signup

---
+++ frontend/src/SignUp/SignUp.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default function SignUp() {
+  return (
+    <div className="container p-5 mt-5 border border-black col-md-5 ">
+      <h3 className="text-center pb-3 border-bottom border-grey">Signup</h3>
+      <div className="row container">
+        <form className="col-md-12  ">
+          <div className="mb-3 mt-4">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className=" mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text"></div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+
+          <div className="container text-center pt-4 ">
+            <button type="submit" className="btn btn-primary px-5 ">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
