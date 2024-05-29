f5e75901bfc8c7225e32baaf90bf153658325f10
yilma <yilma@gmail.com>
2024-05-28 23:14:07 -0400

    signin pages pushed

---
+++ frontend/src/Pages/SigninPage/SigninPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from "../../Components/Header/Header"
+import SignIn from '../../Components/SignIn/SignIn'
+
+export default function SigninPage() {
+  return (
+    <div>
+        <Header></Header>
+        <SignIn></SignIn>
+    </div>
+  )
+}
