dc67ca2423c11d4c8521b1e8b8f7bec23ea3cc52
Sissai <139825952+Sissai@users.noreply.github.com>
2024-05-28 22:50:18 -0400

    Created signup page trial I

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,11 @@ import Note from "./Components/Note/Note";
 import SearchBar from "./Components/Search/SearchBar";
 import SignUp from "./Components/SignUp/SignUp";
 import SignIn from "./Components/SignIn/SignIn";
-
+import SignUpPage from "./Pages/Signup/Signup";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <SignIn />
-      <SearchBar />
-      <Note />
-      <SignUp />
+      <SignUpPage></SignUpPage>
     </div>
   );
 }
